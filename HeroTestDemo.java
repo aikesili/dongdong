69533170ffdfc0bbd82f31620dbe6942e72fdb53
aikesili <1052685255@qq.com>
2016-10-14 17:01:49 +0800

    我的第二个文件

---
+++ HeroTestDemo.java
@@ -0,0 +1,23 @@
+/*1、网络版英雄对打
+	要求：
+	1、定义一个Hero的抽象类
+	2、每个用户有2个英雄，例如：一个是法师（剑），一个是猎人（斧头）
+	3、攻击时，用户可以选择使用的英雄和被攻击的英雄（也可以随机）
+	4、任意一方两个英雄都牺牲了，结束战斗，并将结果保存到本地文件
+	5、三盘两胜制
+
+	技术点：
+	1、使用传输层协议（TCP或者UDP）实现数据的通讯
+	2、使用IO流保存比分结果
+
+	自学知识点：
+	如何将程序编程一个jar包，双击即可运行
+*/
+import java.io.*;
+import java.net.*;
+import java.util.*;
+public class HeroTestDemo{
+	public static void main(String[] args) {
+		
+	}
+}
